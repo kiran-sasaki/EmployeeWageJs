dc228d9f3ffea7c70ae81194330a3f4324baf568
kiran-sasaki <kiransekar1111@gmail.com>
2023-01-26 19:11:13 +0530

    [Kiran] Add: if function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,7 +3,8 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOURS = 20;
-const NUM_OF_WORKING_DAYS=20;
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
 function getWorkingHours(empCheck) {
     switch (empCheck) {
         case IS_PART_TIME:
@@ -14,10 +15,13 @@ function getWorkingHours(empCheck) {
             return 0;
     }
 }
-let totalEmpHrs=0;
-for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs+= getWorkingHours(empCheck);
+    totalEmpHrs += getWorkingHours(empCheck);
 }
+
 let empWage = totalEmpHrs * WAGE_PER_HOURS;
-console.log("Hours : " +totalEmpHrs +" Emp Wage : "+empWage);
+console.log("Total Days : " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Emp Wage : " + empWage);
